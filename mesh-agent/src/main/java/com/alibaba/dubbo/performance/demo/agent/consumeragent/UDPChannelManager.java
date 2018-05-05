6e960d77e8351143346b4a0757753a74d656e162
jinrongtong <794220751@qq.com>
2018-05-05 12:04:41 +0800

    change something

--- mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/consumeragent/UDPChannelManager.java
+++ mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/consumeragent/UDPChannelManager.java
@@ -2,6 +2,7 @@ package com.alibaba.dubbo.performance.demo.agent.consumeragent;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
+import io.netty.channel.epoll.EpollDatagramChannel;
 import io.netty.channel.epoll.EpollEventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioDatagramChannel;
@@ -28,7 +29,7 @@ public class UDPChannelManager {
                             //.group(new NioEventLoopGroup())
                             //.channel(NioDatagramChannel.class)
                             .group(new EpollEventLoopGroup())
-                            .channel(NioDatagramChannel.class)
+                            .channel(EpollDatagramChannel.class)
                             //.option(ChannelOption.SO_BACKLOG, 128)    //设置缓存队列
                             //.option(ChannelOption.SO_RCVBUF, 1024 * 1024)// 设置UDP读缓冲区为1M
                             //.option(ChannelOption.SO_SNDBUF, 1024 * 1024)// 设置UDP写缓冲区为1M ;
