e735aec337b800204e8b231abfae2d5b7f31202a
jinrongtong <794220751@qq.com>
2018-05-12 18:51:15 +0800

    some error to solve

--- mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/provideragent/RpcMsgHandler.java
+++ mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/provideragent/RpcMsgHandler.java
@@ -3,9 +3,11 @@ package com.alibaba.dubbo.performance.demo.agent.provideragent;
 import com.alibaba.dubbo.performance.demo.agent.provideragent.model.RpcResponse;
 import com.alibaba.fastjson.JSON;
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.CompositeByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.socket.DatagramPacket;
+import io.netty.util.CharsetUtil;
 
 import java.net.InetSocketAddress;
 
@@ -18,14 +20,20 @@ public class RpcMsgHandler extends SimpleChannelInboundHandler<ByteBuf> {
     protected void channelRead0(ChannelHandlerContext ctx, ByteBuf byteBuf) throws Exception {
         byteBuf.retain();
         ByteBuf idBuf=byteBuf.slice(0,8);
-        ByteBuf resBuf=byteBuf.slice(12,byteBuf.readableBytes()-12);
-        ByteBuf sendBuf= ctx.alloc().compositeBuffer();
+        byteBuf.readerIndex(12);
+        byte[] res=new byte[byteBuf.readableBytes()];
+        byteBuf.readBytes(res);
+        System.out.println("-------------------------");
+        System.out.println(new String(res));
+        System.out.println("-------------------------");
+        CompositeByteBuf sendBuf= ctx.alloc().compositeBuffer();
         ByteBuf hashCodeBuf=ctx.alloc().ioBuffer();
-        hashCodeBuf.writeBytes(JSON.parseObject(resBuf.array(),Integer.class).toString().getBytes());
+        hashCodeBuf.writeBytes(JSON.parseObject(res,Integer.class).toString().getBytes());
+        sendBuf.addComponents(true,idBuf,hashCodeBuf);
         //这边的ip地址可能有问题
-        DatagramPacket dp = new DatagramPacket(byteBuf,ProviderAgent.getMsgReturner());
+        DatagramPacket dp = new DatagramPacket(sendBuf,ProviderAgent.getMsgReturner());
         //System.out.println(ProviderAgent.getMsgReturner().getHostString()+":"+ProviderAgent.getMsgReturner().getPort());
-        ProviderAgent.getUDPChannel().writeAndFlush(dp).addListener(cf->{
+        ProviderAgent.getUDPChannel().write(dp).addListener(cf->{
             if(!cf.isSuccess()){
                 System.err.println("err in back to consumer agent");
                 cf.cause().printStackTrace();
@@ -35,7 +43,7 @@ public class RpcMsgHandler extends SimpleChannelInboundHandler<ByteBuf> {
 
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-
+        ProviderAgent.getUDPChannel().flush();
     }
 
     @Override
