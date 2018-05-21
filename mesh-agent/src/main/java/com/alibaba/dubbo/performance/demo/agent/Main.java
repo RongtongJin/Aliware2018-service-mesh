43b0307ce46cdc667faa6aca330aca73db69bf1d
jinrongtong <794220751@qq.com>
2018-05-21 12:24:24 +0800

    Test

--- mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/Main.java
+++ mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/Main.java
@@ -11,9 +11,9 @@ public class Main {
         String type = System.getProperty("type");   // 获取type参数
         int port = Integer.valueOf(System.getProperty("server.port"));  //获取端口号
         if ("consumer".equals(type)){
-            new TcpConsumerAgent().start(port);
+            new UdpConsumerAgent().start(port);
         }else if("provider".equals(type)){
-            new TcpProviderAgent().start(port);
+            new UdpProviderAgent().start(port);
         }else {
             System.err.println("Environment variable type is needed to set to provider or consumer.");
         }
